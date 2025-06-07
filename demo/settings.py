414c84ceafbe086aa006ca696b804d861823994d
Shinyylel <cwm3low@gmail.com>
2025-06-07 13:29:01 -0500

    added static pages

--- demo/settings.py
+++ demo/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
