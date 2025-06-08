9e321101dd9ae1c7a466d117faaf160e5c0684f5
Shinyylel <cwm3low@gmail.com>
2025-06-07 19:52:41 -0500

    fix

--- demo/settings.py
+++ demo/settings.py
@@ -37,16 +37,6 @@ INSTALLED_APPS = [
     'myapp',
 ]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
 ROOT_URLCONF = 'demo.urls'
 
 TEMPLATES = [
